6e3f2d227227a812157ecd9ac46011a19df8aceb
Hagar Magdy <hager_magdy@madarsoft.com>
2022-10-12 13:55:52 +0200

    update readme file

--- app/src/main/java/com/example/openlibraryarticles/Config.kt
+++ app/src/main/java/com/example/openlibraryarticles/Config.kt
@@ -4,12 +4,9 @@ class Config {
     companion object
     {
         val  baseUrl = "https://api.nytimes.com/svc/mostpopular/v2/mostviewed/"
-        val  imageBaseUrl = "https://image.tmdb.org/t/p/w500"
         val  apiKeyValue = "KtP5GqbIMlTb00eVeb9amgaSRLh8c3kr"
         // queries keys
         val  apiKey = "api-key"
-        val  PAGE = "page"
-        val  withGenres = "with_genres"
-        val  QUERY = "query"
+
     }
 }
